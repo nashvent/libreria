4baefa5c1a398af303d835f1a32540bb98a619f0
nashvent <anibal.ventura@ucsp.edu.pe>
2017-03-16 22:49:00 -0500

    conexion con navegador

--- pedido.py
+++ pedido.py
@@ -3,8 +3,10 @@ from PyQt5.QtWidgets import QMessageBox, QMainWindow, QApplication, QWidget, QAc
 from PyQt5 import uic
 from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtCore import QDate
 import csv, operator
-
+import time
+import webbrowser
 productos=[]
 with open('productos/productos.csv') as csvarchivo:
     entrada = csv.DictReader(csvarchivo)
@@ -47,6 +49,7 @@ class Lista(QDialog):
         
 class Pedido(QMainWindow):
     def __init__(self,lista):
+        self.documento=Documento()
         self.tamPedido=0
         self.TotalPedido=0
         self.lista=lista
@@ -56,6 +59,7 @@ class Pedido(QMainWindow):
         self.botonAgregar.clicked.connect(self.agregarProducto)
         self.quitarProducto.clicked.connect(self.productoRemovido)
         self.cancelarPedido.clicked.connect(self.limpiarPedido)
+        self.enviarPedido.clicked.connect(self.generarPedido)
     def mostrarLista(self):
         self.lista.show()
 
@@ -103,23 +107,57 @@ class Pedido(QMainWindow):
         self.tamPedido=0
         self.tablePedido.setRowCount(self.tamPedido)
         self.cantidadPedido.setValue(1)
-
+    def generarPedido(self):
+        if(self.tamPedido>0):
+            self.documento.actualizarTabla(self.tablePedido,self.TotalPedidoLabel.text())
+        else:
+            QMessageBox.question(self, "ALERTA", "No agregaste ningun producto.", QMessageBox.Ok)
+        
 class Documento(QMainWindow): 
     def __init__(self):
         super().__init__()
         uic.loadUi("ui/documento.ui", self)
-        self.initUI()
-        
+        #self.initUI()
+        self.tipoDocumento.currentTextChanged.connect(self.itemChanged)
+        #print (time.strftime("%I:%M:%S"))
+        #print (time.strftime("%d/%m/%y"))
+        self.fechaDocumento.setDate(QDate.currentDate())
+        self.generarDocumento.clicked.connect(self.generarDoc)
     def initUI(self):
         self.show()
         
+
+    def actualizarTabla(self,lista,total):
+        self.tablaVenta.setRowCount(lista.rowCount())
+        for i in range(lista.rowCount()):
+            for j in range(5):
+                self.tablaVenta.setItem(i,j,QTableWidgetItem(lista.item(i,j).text()))
+        self.totalCobro.setText(total)
+        self.show()
+
+    def itemChanged(self):
+        #print("Seleccionado: ", self.tipoDocumento.currentText())
+        if(self.tipoDocumento.currentText()=='Factura'):
+            self.documentoLabel.setText('RUC:')
+        else:
+            self.documentoLabel.setText('DNI:')
+
+    def generarDoc(self):
+        #archi=open('datos.html','w')
+        archi=open('datos.html','a')
+        archi.write(self.nombreDocumento.text())
+        archi.write(self.numeroDocumento.text())
+        archi.write(self.direccionDocumento.text())
+        archi.close()
+        webbrowser.get("C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s").open("file:///C:/Users/Nash/Documents/NACHO FINAL/datos.html")
+
         
 app = QApplication(sys.argv)
 lista=Lista()
 documento=Documento()
 pedido = Pedido(lista)
 lista.pedido=pedido
-
+pedido.documento=documento
 pedido.show()
 app.exec_()
 
